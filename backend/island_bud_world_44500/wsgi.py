17ff8d9851322d277418928f2cf332accc49be48
Crowdbotics <team@crowdbotics.com>
2023-11-12 21:19:51 +0000

    Initial commit

---
+++ backend/island_bud_world_44500/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for island_bud_world_44500 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "island_bud_world_44500.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "island-bud-world-44500",
+  url: "https://island-bud-world-44500.botics.co",
+  api: "https://island-bud-world-44500.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
